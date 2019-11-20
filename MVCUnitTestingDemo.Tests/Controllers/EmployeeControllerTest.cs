8bfae3678eb12a3c70a7728820aeeb78f9fee906
Jignesh Darji <jignesh.darji@sedemac.com>
2019-11-20 12:40:51 +0530

    Initial Commit

---
+++ MVCUnitTestingDemo.Tests/Controllers/EmployeeControllerTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MVCUnitTestingDemo.Controllers;
+using MVCUnitTestingDemo.Models;
+
+namespace MVCUnitTestingDemo.Tests.Controllers
+{
+    [TestClass]
+    public class EmployeeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            //Arrange
+            EmployeeController employeeController = new EmployeeController();
+            //Act
+            ViewResult result = employeeController.Index() as ViewResult;
+            //Assert
+            Assert.IsNotNull(result);
+        }
+        [TestMethod]
+        public void Employees()
+        {
+            //Arrange
+            EmployeeController employeeController = new EmployeeController();
+            //Act
+            ViewResult result = employeeController.Employees() as ViewResult;
+            //Assert
+            Assert.IsNotNull(result);
+        }
+        [TestMethod]
+        public void NoOfEmployeesIs4()
+        {
+            //Arrange
+            EmployeeController employeeController = new EmployeeController();
+            //Act
+           List<Employee> employeelist= employeeController.GetEmployeeList() as List<Employee>;
+            //Assert
+            Assert.IsTrue(employeelist.Count == 4);
+        }
+
+    }
+}
---
+++ MVCUnitTestingDemo/Controllers/EmployeeController.cs
@@ -0,0 +1,61 @@
+﻿using MVCUnitTestingDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCUnitTestingDemo.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Employees()
+        {
+            var employees = from e in GetEmployeeList()
+                            orderby e.ID
+                            select e;
+            return View(employees);
+        }
+        [NonAction]
+        public List<Employee> GetEmployeeList()
+        {
+            return new List<Employee>()
+            {
+                new Employee()
+                {
+                    ID=1,
+                    Age=21,
+                    JoiningDate= DateTime.Now,
+                    Name ="A"
+                },
+                new Employee()
+                {
+                    ID=2,
+                    Age=22,
+                    JoiningDate= DateTime.Now,
+                    Name ="B"
+                },
+                new Employee()
+                {
+                    ID=3,
+                    Age=23,
+                    JoiningDate= DateTime.Now,
+                    Name ="C"
+                },
+                new Employee()
+                {
+
+                    ID=4,
+                    Age=24,
+                    JoiningDate= DateTime.Now,
+                    Name ="D"
+                },
+            };
+        }
+    }
+}
